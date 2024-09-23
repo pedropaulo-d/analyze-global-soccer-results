c470544a0d46a6cfe0f17c30f19f4ec2b15b89a1
Pedro <paulopedro.dev@gmail.com>
2024-09-23 10:14:10 -0400

    Adiciona uma analise de vitorias de jogos em casa, fora, neutro ou empate.

---
+++ codes/win_casaxfora.py
@@ -0,0 +1,48 @@
+# %%
+import pandas as pd
+
+df_matches = pd.read_csv('../data/Match_Results.csv', sep=',')
+
+df_matches
+# %%
+def tipo_vitoria(row):
+    if row['home_score'] > row['away_score']:  # Vitória do time da casa
+        if row['neutral']:  # Se foi em campo neutro
+            return 'Vitória em Campo Neutro'
+        else:
+            return 'Vitória em Casa'
+    elif row['home_score'] < row['away_score']:  # Vitória do time visitante
+        if row['neutral']:  # Se foi em campo neutro
+            return 'Vitória em Campo Neutro'
+        else:
+            return 'Vitória Fora de Casa'
+    else:
+        return 'Empate'
+    
+df_matches['tipo_vitoria'] = df_matches.apply(tipo_vitoria, axis=1)
+
+df_matches
+# %%
+df_qtd_win = df_matches.groupby(['tipo_vitoria']).agg({
+
+    'date': 'count',
+
+}).reset_index()
+
+# %%
+import plotly.express as px
+
+fig = px.bar(
+            df_qtd_win, 
+            x='tipo_vitoria', 
+            y='date',
+            color='tipo_vitoria',
+            title='Análise de Vitórias em Casa vs Fora',
+            labels={
+                    'value': 'Número de Vitórias',
+                    'variable': 'Tipo de Jogo',
+                    'date': 'N° de Vitórias',
+                    'tipo_vitoria': 'Tipo de Vitória'
+                    })
+fig.show()
+# %%
