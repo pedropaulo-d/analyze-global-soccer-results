7eb1b2bc172829e12bd9dcc746a1bc0de1fc3ce3
Pedro <paulopedro.dev@gmail.com>
2024-09-27 08:52:29 -0400

    Adiciona uma analise do total de gols feitos por penaltis e contras por ano

---
+++ codes/gols_contras_penaltis.py
@@ -0,0 +1,87 @@
+# %%
+import pandas as pd
+
+df_goal = pd.read_csv('../data/Goal_Scorers.csv', sep=',')
+df_goal
+# %%
+
+df_goal['date'] = pd.to_datetime(df_goal['date']).dt.year
+df_goal
+# %%
+
+df_qtd_gols_contras = (df_goal.groupby(['date'])
+                .agg(
+                    qtd_gol_contra=('own_goal', 'sum')
+                    )
+                .reset_index())
+
+df_qtd_gols_contras
+# %%
+
+import plotly.express as px
+
+
+fig = px.line(df_qtd_gols_contras, x='date', y='qtd_gol_contra', 
+              title='Quantidade total de gols contra por Ano',
+              labels={
+                  
+                  'qtd_gol_contra': 'Total de gols contra',
+                  'date': 'Ano',
+                  'value': 'Total de gols',
+                  'variable':'Tipo de Métrica'
+                  
+                  })
+fig.show()
+
+# %%
+df_penalty = pd.read_csv('../data/Goal_Scorers.csv', sep=',')
+df_penalty
+# %%
+
+df_penalty['date'] = pd.to_datetime(df_penalty['date']).dt.year
+df_penalty
+# %%
+
+df_qtd_gols_penaltis = (df_penalty.groupby(['date'])
+                .agg(
+                    qtd_gol_penaltis=('penalty', 'sum')
+                    )
+                .reset_index())
+
+df_qtd_gols_penaltis
+# %%
+
+fig = px.line(df_qtd_gols_penaltis, x='date', y='qtd_gol_penaltis', 
+              title='Quantidade total de gols de Penaltis por ano',
+              labels={
+                  
+                  'qtd_gol_penaltis': 'Total de gols de Penaltis',
+                  'date': 'Ano',
+                  'value': 'Total de Gols',
+                  'variable':'Tipo de Métrica'
+                  
+                  })
+fig.show()
+# %%
+
+df_gols_penaltis_contra = pd.merge(df_qtd_gols_contras, df_qtd_gols_penaltis, left_on='date', right_on='date')
+df_gols_penaltis_contra.rename(columns={'qtd_gol_contra': 'Gols Contra', 'qtd_gol_penaltis': 'Gols de Penaltis'}, inplace=True)
+# %%
+
+fig = px.line(df_gols_penaltis_contra, x='date', y=['Gols de Penaltis', 'Gols Contra'], 
+              title='Quantidade total de gols de Penaltis e Contra por ano',
+              labels={
+                  
+                  'qtd_gol_penaltis': 'Total de gols de Penaltis e Contra',
+                  'date': 'Ano',
+                  'value': 'Total de Gols',
+                  'variable':'Tipo de Métrica',
+                 
+                  })
+fig.show()
+# %%
+
+html_string = fig.to_html(full_html=True, include_plotlyjs='cdn')
+with open("gols_contra_penaltis.html", "w") as f:
+    f.write(html_string)
+# %%
